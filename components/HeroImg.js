95f587de03a80a04e144cfe57353672a2aa05f83
Noversal <dionofrio_franco@hotmail.com>
2023-04-18 02:59:48 -0300

    fix text heroImg

--- components/HeroImg.js
+++ components/HeroImg.js
@@ -5,11 +5,8 @@ export default function HeroImg ({ img, text }) {
 
   return /* html */`
   <div class="slideshow-container">
-  <div class = "text-hero">       
-  ${text}
-  </div>
-  <img src=${rutaImg} style="height:100%;width:100%;object-fit:cover;">
-   
+    <div class="text-hero">${text}</div>
+    <img src=${rutaImg} style="height:100%;width:100%;object-fit:cover;">
   </div>
     `
 }
--- listeners/heroCarrousel.js
+++ listeners/heroCarrousel.js
@@ -2,13 +2,14 @@ import $ from 'jquery'
 
 function showNextSlide ({ parrafo, images, currentIndex }) {
 // Ocultar la diapositiva actual
-  images[currentIndex].style.opacity = 0
-  parrafo[currentIndex].style.opacity = 0
+  $(images[currentIndex]).css({ opacity: 0 })
+  $(parrafo[currentIndex]).css({ opacity: 0 })
   // Calcular el índice de la próxima diapositiva
   currentIndex = (currentIndex + 1) % images.length
   // Mostrar la próxima diapositiva
-  images[currentIndex].style.opacity = 1
-  parrafo[currentIndex].style.opacity = 1
+  $(images[currentIndex]).css({ opacity: 1 })
+  $(parrafo[currentIndex]).css({ opacity: 1 })
+
   return currentIndex
 }
 
@@ -17,15 +18,14 @@ export function heroCarrousel () {
   const parrafo = $('.slideshow-container .text-hero')
   const slideDuration = 5500 // Duración de cada diapositiva en milisegundos
 
-  // Mostrar la primera diapositiva
-
-  // Cambiar de diapositiva automáticamente
   let currentIndex = 0
 
+  if (currentIndex === 0) {
+    $(images[currentIndex]).css({ opacity: 1 })
+    $(parrafo[currentIndex]).css({ opacity: 1 })
+  }
+
   setInterval(() => {
-    console.log(currentIndex)
-    images[currentIndex].style.opcaity = 1
-    parrafo[currentIndex].style.opacity = 1
     currentIndex = showNextSlide({ parrafo, images, currentIndex })
   }, slideDuration)
 }
